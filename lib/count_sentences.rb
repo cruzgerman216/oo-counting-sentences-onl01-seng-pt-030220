4409de45a056fa1af940c20818a31168ff6a91fb
German Cruz <cruzgerman216@yahoo.com>
2020-03-14 19:05:29 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,7 +1,6 @@
 require 'pry'
 
 class String
-
   def sentence?
     self.end_with?(".")
   end
@@ -12,10 +11,13 @@ class String
 
   def exclamation?
     self.end_with?("!")
-
   end
-
+  
   def count_sentences
-    self.split(" ").count
+    str = self.split(" ").count
+    str
   end
 end
+
+
+puts "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
